5af6303e5243c7f99df180890a52256cea8a3fbd
Beatriz <biacris5@yahoo.com.br>
2018-02-21 11:57:52 -0300

    Apresenta o init e chama a decisão #24

--- src/joia.py
+++ src/joia.py
@@ -1,3 +1,21 @@
-    class joia:
-        def envia(self):
-            pass
+class joia:
+    """
+    Recompensa recebida no jogo do tesouro Inca
+    """
+    def __init__(self,decisão):
+        """
+        Construção do objeto joia
+
+        :param decisão: Uma referência para o objeto decisão
+        """
+        self.decisão = decisão
+    def envia(self):
+        """
+        As joias são distribuídas chamando este método
+
+        Após o envio das joias a decisão deve ser tomada
+        :return:
+        """
+        self.decisão()
+        pass
+
