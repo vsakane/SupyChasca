741c50e86a681f877a5873ebca82a44a23dd4c3e
Dayara <dayara.s.carvalho@gmail.com>
2018-02-21 11:55:03 -0300

    Acrescenta o oniti e chama jogador sai

--- src/decisao.py
+++ src/decisao.py
@@ -1,3 +1,11 @@
 class Decisao:
+    """
+    Carta que o jogador usa para continuar ou sair do jogo.
+    """
     def decide(self):
+        def__init
+        """"
+        o Jogador usa essa carta para sair ou continuar no jogo
+        :return:
+        """
         pass
