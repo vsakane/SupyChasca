4a6d92cf95195d78b39ea169111e724cd5306c9a
Carlo <carlo@ufrj.br>
2018-02-21 11:50:43 -0300

    Acrescenta o init e chama o templo closes #28

--- src/jogo.py
+++ src/jogo.py
@@ -1,3 +1,19 @@
 class Jogo:
+    """
+    O módulo principal do jogo do tesouro Inca.
+    """
+    def __init__(self, templo):
+        """
+        Construção do objeto Jogo.
+
+        :param templo: Uma referência para o objeto Templo.
+        """
+        self.templo = templo
     def inicia(self):
-        pass
+        """
+        O jogo do Tesouro Inca começa chamando este método
+
+        Ao iniciar o jogo o Templo deve ser montado
+        :return:
+        """
+        self.templo.montar()
