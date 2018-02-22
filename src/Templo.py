54b58a6f27052ad9ccf1e7c13ee3d8c7d577e285
ellesimas <ellesimas@gmail.com>
2018-02-22 18:03:58 -0300

    desenvolvimento do projeto templo ...

--- src/Templo.py
+++ src/Templo.py
@@ -1,3 +1,16 @@
     class Templo:
-        def templo(self):
+        """
+        O templo é representado por 5 cartas que reserva os artefatos.
+
+        Até o final do jogo todos os artefatos em baixo serão retirados.
+        """
+        def __init__:(self,pass)
+        def templo(self, inca_templo):
+            """
+            Guarda os artefatos.
+
+            :param inca_templo: guarda os artefatos.
+            :return: Nada
+            """
+
             pass
