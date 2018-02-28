250ffc617a47ce7374fa702dc1ed1deb58584536
carlotolla <carlo@nce.ufrj.br>
2018-02-28 11:38:15 -0300

    Consertou templo closes #74

--- src/Templo.py
+++ src/Templo.py
@@ -1,26 +1,29 @@
-    class Templo:
+class Templo:
+    """
+    O templo é representado por 5 cartas que reserva os artefatos.
+
+    Até o final do jogo todos os artefatos em baixo serão retirados.
+    """
+    def __init__(self, baralho):
+        self.baralho = baralho
+
+    def montar(self, inca_templo):
         """
-        O templo é representado por 5 cartas que reserva os artefatos.
+        Guarda os artefatos.
 
-        Até o final do jogo todos os artefatos em baixo serão retirados.
+        :param inca_templo: guarda os artefatos.
+        :return: Nada
         """
-        def __init__:(self,pass)
-        def templo(self, inca_templo):
-            """
-            Guarda os artefatos.
 
-            :param inca_templo: guarda os artefatos.
-            :return: Nada
-            """
+        pass
 
-            pass
-        def virardegrau(self):
-            """
-            Templo mostra Artefato 1
-            Templo mostra Artefato 2
-            Templo mostra Artefato 3
-            Templo mostra Artefato 4
-            Templo mostra Artefato 5
-            :return: Baralho
-            """
-            self.baralho.embaralha
+    def virardegrau(self):
+        """
+        Templo mostra Artefato 1
+        Templo mostra Artefato 2
+        Templo mostra Artefato 3
+        Templo mostra Artefato 4
+        Templo mostra Artefato 5
+        :return: Baralho
+        """
+        self.baralho.embaralha()
