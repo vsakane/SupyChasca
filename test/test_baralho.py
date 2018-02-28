fb3c45333f075490540a22961f31367f9ddec328
Sara Hellen <sarinha.matos.sm@gmail.com>
2018-02-28 18:47:39 -0300

    Teste embaralhou. closes #78

--- test/test_baralho.py
+++ test/test_baralho.py
@@ -12,3 +12,11 @@ class BaralhoTeste(unittest.TestCase):
         # testar se o baralho contêm o artefato recebido
         assert baralho == baralho2, (baralho, baralho2)
         assert artefato in baralho.deque_de_cartas
+
+    def testa_embaralha(self):
+        baralho = Baralho()
+        lista = baralho.deque_de_cartas [:]
+        baralho.embaralha ()
+        lista1 = baralho.deque_de_cartas
+        assert lista1 != lista, (lista1, lista)
+        
