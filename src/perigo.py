2e64dcc4809c55c87894272061e7da704c12f546
Sara Hellen <sarinha.matos.sm@gmail.com>
2018-02-28 10:13:45 -0300

    Define Perigo apresentese

---
+++ src/perigo.py
@@ -0,0 +1,14 @@
+class Perigo :
+    """
+    O que faz o jogador decidir ou perder
+    """
+    def __init__(self):
+        pass
+    def apresentese (self) :
+        """
+        O perigo se apresenta
+        :return: chamar jogador
+
+        """
+        self.perigo.apresentese()
+        pass
