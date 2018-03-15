904fb87f5d88da86dff612ee07720d6bec34d088
veronica <ve.sm@hotmail.com>
2018-03-15 09:03:07 -0300

    alteracao basica de demontracao, proximo passo fazer a mesa receber as cartas. colocar input de continue ou sai

--- src/jogo.py
+++ src/jogo.py
@@ -1,16 +1,17 @@
 from Templo import Templo
-
+from baralho import Baralho
 
 class Jogo:
     """
     O módulo principal do jogo do tesouro Inca.
     """
-    def __init__(self, templo):
+    def __init__(self, templo = None):
         """
         Construção do objeto Jogo.
 
         :param templo: Uma referência para o objeto Templo.
         """
+        self.baralho = Baralho()
         self.templo = templo
 
     def inicia(self):
@@ -20,8 +21,10 @@ class Jogo:
         Ao iniciar o jogo o Templo deve ser montado
         :return:
         """
-        self.templo.montar()
+        #self.templo.montar()
+        self.baralho.embaralha()
+        print (self.baralho.deque_de_cartas)
 
 
 if __name__ == "__main__":
-    Jogo(Templo()).inicia()
+    Jogo().inicia()
